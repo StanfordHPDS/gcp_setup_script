28761dded11247b2b44f1eb9c922d1fd9620d1be
Malcolm Barrett <malcolmbarrett@gmail.com>
2024-11-04 14:17:21 -0500

    no really fix typo

--- setup.sh
+++ setup.sh
@@ -157,5 +157,5 @@ run_with_spinner "Setting default Git branch to main" "git config --global init.
 echo -e "\n${BOLD}Installation complete!${RESET} Log saved to ${GREEN}${LOG_FILE}${RESET}."
 echo "${BOLD}Installed tools${RESET}: R, Python, Quarto, RStudio Server, VS Code, DuckDB, Miniconda, Rust, uv"
 echo -e "${BOLD}To finalize setup:${RESET} open a new terminal or log out and back in to update paths."
-echo -e "${BOLD}Forward ports for RStudio Server (8787) and VS Code (8080) with${RESET}: \
-  '${GREEN}gcloud compute ssh --zone \"\$ZONE\" \"\$INSTANCE_NAME\" --project \"\$PROJECT_ID\'" -- -L 8787:localhost:8787 -L 8080:localhost:8080${RESET}"
+echo -e "${BOLD}Forward ports for RStudio Server (8787) and VS Code (8080) with${RESET}:
+  '${GREEN}gcloud compute ssh --zone \"\$ZONE\" \"\$INSTANCE_NAME\" --project \"\$PROJECT_ID\" -- -L 8787:localhost:8787 -L 8080:localhost:8080${RESET}'"
