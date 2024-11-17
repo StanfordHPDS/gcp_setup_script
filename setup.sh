7804d7124883f2eaba258c04cc069b8bdb47fef1
Malcolm Barrett <malcolmbarrett@gmail.com>
2024-11-17 17:25:08 -0500

    add ruff ext

--- setup.sh
+++ setup.sh
@@ -147,7 +147,8 @@ run_with_spinner "Installing VS Code extensions" "
   code-server --install-extension ms-python.python && \
   code-server --install-extension ms-toolsai.jupyter && \
   code-server --install-extension quarto.quarto && \
-  code-server --install-extension sqlfluff.sqlfluff
+  code-server --install-extension sqlfluff.sqlfluff && \
+  code-server --install-extension charliermarsh.ruff
 "
 
 # 13c. Configure Quarto settings in VS Code
